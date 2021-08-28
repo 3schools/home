dbae03b6bc9a10280b34b93e9a246b7cb3c993c5
3schools <85482920+3schools@users.noreply.github.com>
2021-08-28 19:54:09 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,16 +1,1 @@
-self.addEventListener('install', (e) => {
-  e.waitUntil(
-    caches.open('quiz-schools').then((cache) => cache.addAll([
-      'https://shortenersurl.blogspot.com/2021/08/home.html' ,
-      'https://raw.githubusercontent.com/3schools/3schools/main/3schools.png',
-      'https://raw.githubusercontent.com/3schools/home/main/index.js',
-    ])),
-  );
-});
-
-self.addEventListener('fetch', (e) => {
-  console.log(e.request.url);
-  e.respondWith(
-    caches.match(e.request).then((response) => response || fetch(e.request)),
-  );
-}); 
+ console.log("new sw");

7b978ac274095a4add6869ad31d7c65da2ed8ea8
3schools <85482920+3schools@users.noreply.github.com>
2021-08-28 21:52:18 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,103 +1,54 @@
- "use strict";
 
-const notificationButton = document.getElementById("enableNotifications");
-let swRegistration = null;
-const TokenElem = document.getElementById("token");
-const ErrElem = document.getElementById("err");
-
-// Initialize Firebase
-// TODO: Replace with your project's customized code snippet
-const config = {
-  apiKey: "AIzaSyAe2sb36Zcs4WydOfr4fKxhQ3DqBFY_7Sc",
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  const firebaseConfig = {
+    apiKey: "AIzaSyAe2sb36Zcs4WydOfr4fKxhQ3DqBFY_7Sc",
     authDomain: "ask-schools.firebaseapp.com",
     projectId: "ask-schools",
     storageBucket: "ask-schools.appspot.com",
     messagingSenderId: "760204072900",
     appId: "1:760204072900:web:2872651c677c29144f3691"
-};
-firebase.initializeApp(config);
-const messaging = firebase.messaging();
-initializeApp();
-
-function initializeApp() {
-  if ("serviceWorker" in navigator && "PushManager" in window) {
-    console.log("Service Worker and Push is supported");
-    initializeUi();
-    initializeFCM();
-
-    //Register the service worker
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(swReg => {
-        console.log("Service Worker is registered", swReg);
-        swRegistration = swReg;
-      })
-      .catch(error => {
-        console.error("Service Worker Error", error);
-      });
-    navigator.serviceWorker.ready.then(function(registration) {
-      console.log("A service worker is active:", registration.active);
-
-      // At this point, you can call methods that require an active
-      // service worker, like registration.pushManager.subscribe()
-    });
-  } else {
-    console.warn("Push messaging is not supported");
-    notificationButton.textContent = "Push Not Supported";
-  }
-}
-
-function initializeUi() {
-  notificationButton.addEventListener("click", () => {
-    displayNotification();
-  });
-}
-
-function initializeFCM() {
-  messaging
-    .requestPermission()
-    .then(() => {
-      console.log("Notification permission granted.");
-
-      // get the token in the form of promise
-      return messaging.getToken();
-    })
-    .then(token => {
-      TokenElem.innerHTML = "token is : " + token;
-    })
-    .catch(err => {
-      ErrElem.innerHTML = ErrElem.innerHTML + "; " + err;
-      console.log("Unable to get permission to notify.", err);
-    });
-}
-
-function displayNotification() {
-  if (window.Notification && Notification.permission === "granted") {
-    notification();
-  }
-  // If the user hasn't told if he wants to be notified or not
-  // Note: because of Chrome, we are not sure the permission property
-  // is set, therefore it's unsafe to check for the "default" value.
-  else if (window.Notification && Notification.permission !== "denied") {
-    Notification.requestPermission(status => {
-      if (status === "granted") {
-        notification();
-      } else {
-        alert("You denied or dismissed permissions to notifications.");
-      }
-    });
-  } else {
-    // If the user refuses to get notified
-    alert(
-      "You denied permissions to notifications. Please go to your browser or phone setting to allow notifications."
-    );
-  }
-}
-
-function notification() {
-  const options = {
-    body: "Testing Our Notification",
-    icon: "./bell.png"
   };
-  swRegistration.showNotification("PWA Notification!", options);
-}
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  const auth =  firebase.auth();
+
+  //signup function
+  function signUp(){
+    var email = document.getElementById("email");
+    var password = document.getElementById("password");
+
+    const promise = auth.createUserWithEmailAndPassword(email.value,password.value);
+    //
+    promise.catch(e=>alert(e.message));
+    alert("SignUp Successfully");
+  }
+
+  //signIN function
+  function  signIn(){
+    var email = document.getElementById("email");
+    var password  = document.getElementById("password");
+    const promise = auth.signInWithEmailAndPassword(email.value,password.value);
+    promise.catch(e=>alert(e.message));
+    
+  }
+
+
+  //signOut
+
+  function signOut(){
+    auth.signOut();
+    alert("SignOut Successfully from System");
+  }
+
+  //active user to homepage
+  firebase.auth().onAuthStateChanged((user)=>{
+    if(user){
+      var email = user.email;
+      alert("Active user "+email);
+
+    }else{
+      alert("No Active user Found")
+    }
+  })

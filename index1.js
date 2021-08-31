f2279ae498cb1ae6bd341ca8e51f0cb256ca2198
3schools <85482920+3schools@users.noreply.github.com>
2021-08-31 14:19:58 +0530

    Update index1.js

--- index1.js
+++ index1.js
@@ -21,17 +21,15 @@ function register () {
   email = document.getElementById('email').value
   password = document.getElementById('password').value
   full_name = document.getElementById('full_name').value
-  favourite_song = document.getElementById('favourite_song').value
-  milk_before_cereal = document.getElementById('milk_before_cereal').value
-
+  
   // Validate input fields
   if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is Outta Line!!')
+    alert('Email or Password is empty!')
     return
     // Don't continue running the code
   }
-  if (validate_field(full_name) == false || validate_field(favourite_song) == false || validate_field(milk_before_cereal) == false) {
-    alert('One or More Extra Fields is Outta Line!!')
+  if (validate_field(full_name) == false || validate_field(full_name) == false) {
+    alert('Enter your full name!')
     return
   }
  
@@ -48,8 +46,6 @@ function register () {
     var user_data = {
       email : email,
       full_name : full_name,
-      favourite_song : favourite_song,
-      milk_before_cereal : milk_before_cereal,
       last_login : Date.now()
     }
 
@@ -76,7 +72,7 @@ function login () {
 
   // Validate input fields
   if (validate_email(email) == false || validate_password(password) == false) {
-    alert('Email or Password is Outta Line!!')
+    alert('Email or Password is empty!!')
     return
     // Don't continue running the code
   }
